7210c34536d00f9c3b852e36ea91e799aaac18a8
Joseph Mudd <josephmuddjr@gmail.com>
2023-08-15 14:20:51 -0500

    Add basic operator function

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function multiply(a , b) {
 function divide(a , b) {
     return a / b;
 }
+
+let operandOne
+,   operandTwo
+,   operator;
+
+function operate(operator, numberOne, numberTwo) {
+    switch (operator) {
+        case '+':
+            add(numberOne , numberTwo);
+            break;
+
+        case '-':
+            subtract(numberOne , numberTwo);
+            break;
+
+        case '*':
+            multiply(numberOne , numberTwo);
+            break;
+
+        case '/':
+            divide(numberOne , numberTwo);
+            break;
+    }
+}
