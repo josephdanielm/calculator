ddb7a46523de39247e5bda871d6459fac8aecfa6
Joseph Mudd <josephmuddjr@gmail.com>
2023-08-15 15:56:52 -0500

    Add basic operator and button event listeners

--- script.js
+++ script.js
@@ -37,3 +37,34 @@ function operate(operator, numberOne, numberTwo) {
             break;
     }
 }
+
+
+let displayValue = document.querySelector('#current-operation');
+
+const numberBtns = document.querySelectorAll('.number-button');
+numberBtns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        if (displayValue.textContent === '0') {displayValue.textContent = null}
+        displayValue.textContent += event.target.textContent;
+        console.log(displayValue);
+    })
+}
+)
+
+
+let clearBtn = document.querySelector('#clear-button');
+
+clearBtn.addEventListener('click', event => {
+    displayValue.textContent = 0;
+})
+
+
+const functionBtns = document.querySelectorAll('.functional-button');
+
+functionBtns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        operandOne = displayValue.textContent;
+        operator = String(event.target.textContent);
+        displayValue.textContent = 0;
+    })
+})
